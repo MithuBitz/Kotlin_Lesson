fbf88f8c6903a6472a6c2c230754543da04e294c
MithuBitz <mithubortamuly@gmail.com>
2020-08-21 01:18:12 +0530

    Constrained Layout chain

--- app/src/main/java/com/example/userinterface/MainActivity.kt
+++ app/src/main/java/com/example/userinterface/MainActivity.kt
@@ -17,25 +17,5 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
 
-            fab.setOnClickListener { view ->
-                Snackbar.make(view, "You Entered a name of ${editText.text}", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            }
-        }
-
-        override fun onCreateOptionsMenu(menu: Menu): Boolean {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            menuInflater.inflate(R.menu.menu_main, menu)
-            return true
-        }
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            return when (item.itemId) {
-                R.id.action_settings -> true
-                else -> super.onOptionsItemSelected(item)
-            }
-        }
+    }
 }
