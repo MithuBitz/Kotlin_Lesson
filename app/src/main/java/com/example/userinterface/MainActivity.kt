386d415b9ec5eac21416499685b962e7771f14bd
MithuBitz <mithubortamuly@gmail.com>
2020-08-21 01:34:01 +0530

    Constrained Layout chain

--- app/src/main/java/com/example/userinterface/MainActivity.kt
+++ app/src/main/java/com/example/userinterface/MainActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -16,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        //Call the function to create a TextView
+        addTextView("Hello")
+        addTextView("From")
+        addTextView("Mithu")
 
+    }
+
+    //Create a function to create a TextView through code
+    private fun addTextView(lebel : String){
+        val view = TextView(this)
+        view.text = lebel
+        view.textSize = 28f
+        linearLayout.addView(view)
     }
 }
