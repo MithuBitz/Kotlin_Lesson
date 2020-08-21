4ebb93d15b7d0e377558b0d40232b0f24230cc2c
MithuBitz <mithubortamuly@gmail.com>
2020-08-21 23:57:47 +0530

    change mainactivity.kt file

--- app/src/main/java/com/example/userinterface/MainActivity.kt
+++ app/src/main/java/com/example/userinterface/MainActivity.kt
@@ -19,25 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        button.setOnClickListener{
-            val userName = nameInput.text.toString()
-            val password = passwordInput.text.toString()
-
-            //String concatination useing string.xml file
-            val message = getString(R.string.loginMessage, userName, password)
-
-            //Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-
-            //Useing snakbar for more complex benifits than Toast
-            Snackbar.make(it, message, Snackbar.LENGTH_LONG).setAction("Click me", {showAnotherMessage() }).show()
-        }
-
-
 
     }
 
-    private fun showAnotherMessage() {
-        Toast.makeText(this, "You Clicked", Toast.LENGTH_LONG).show()
-    }
 
 }
